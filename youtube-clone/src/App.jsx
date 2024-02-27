c827a07eb77d48c1b8cbcf15e9ebca8af687be90
NewUser <newuser@MacBook-Pro-2.local>
2024-02-26 19:17:35 -0500

    PlayVideo Done

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -5,12 +5,13 @@ import Home from './Pages/Home/Home'
 import Video from './Pages/Video/Video'
 
 const App = () => {
-
+  console.log()
   const [sidebar, setSidebar] = useState(true);
   console.log(` ${(sidebar?"Default":"small-sidebar")} in App.jsx`)
 
   return (
     <div> 
+
       <Navbar setSidebar={setSidebar}/>
       <Routes>
         <Route path='/'element={<Home sidebar={sidebar}/>  }/>
